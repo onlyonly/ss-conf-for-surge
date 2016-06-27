c3d101f2db9b02d0b481338c68595ebad731bafc
yakiang <git@yakiang.com>
2016-06-27 16:28:34 +0800

    fix adlist

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-    'adlist': 'https://gist.githubusercontent.com/iyee/2e27c124af2f7a4f0d5a/raw/7758f00988cb817b9d5d589e548d804fa61adc51/main.conf',
+    'adlist': 'https://raw.githubusercontent.com/R0uter/ss.conf-for-surge/master/list/adlist.txt',
     'gfwlist': 'https://raw.githubusercontent.com/R0uter/ss.conf-for-surge/master/list/gfwlist.txt',
 
     'template': path.join(__dirname, '..', 'template.conf'),
